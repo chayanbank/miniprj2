41a2c54c58fc6913980ed616f370b5a7767cbe7e
chayanbank <chayan_bang@hotmail.com>
2017-03-22 11:43:57 +0700

    add new

---
+++ Edit.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Edit</title>
+</head>
+<body>
+</body>
+</html>
---
+++ NewNote.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>NewNote</title>
+</head>
+<body>
+</body>
+</html>
